bc8f8ec47ecd6a887d37930936df0980a38c190b
Pedro Januário <prnjanuario@gmail.com>
2014-06-24 18:01:18 +0100

    implemented service management interface as logical service
    Later we could port this to physical service and have async handle of
    protocol messages.

---
+++ spec/unit/smi_spec.js
@@ -0,0 +1,261 @@
+describe("ServiceManagementInterface", function(){
+  var log = require('../../../core/lib/logger'),
+      Message = require('../../../core/lib/message'),
+      uuid = require('uuid'),
+      SMI = require('../../lib/smi');
+
+  var target;
+
+  var config = {
+    // heartbeat interval in ms
+    heartbeat: 100,
+    // max ttl for a service in ms
+    maxTTL: 200,
+    // refresh service ttl update interval in ms
+    updateInterval: 100
+  };
+
+  var heartbeat, up, down;
+
+  beforeEach(function(){
+    spyOn(uuid, 'v1').andReturn("uuid");
+    spyOn(log, 'trace').andReturn(Function.apply());
+    spyOn(log, 'debug').andReturn(Function.apply());
+    spyOn(log, 'info').andReturn(Function.apply());
+    spyOn(log, 'warn').andReturn(Function.apply());
+    spyOn(log, 'error').andReturn(Function.apply());
+
+    // TODO: set on helpers for all tests
+    // issue with clearTimeout/clearInterval: https://github.com/mhevery/jasmine-node/issues/276
+    // spyOn(global, 'clearTimeout').andCallFake(function() {
+    //   return jasmine.Clock.installed.clearTimeout.apply(this, arguments);
+    // });
+
+    spyOn(global, 'clearInterval').andCallFake(function() {
+      return jasmine.Clock.installed.clearInterval.apply(this, arguments);
+    });
+
+    up = new Message("SMI", "UP");
+    up.identity = "service";
+    up.payload = "service-sid";
+
+    down = new Message("SMI", "DOWN");
+    down.identity = "service";
+    down.payload = "service-sid";
+
+    heartbeat = new Message("SMI", "HEARTBEAT");
+    heartbeat.identity = "service";
+    heartbeat.payload = "service-sid";
+
+    jasmine.Clock.useMock();
+
+    target = new SMI(config);
+    target.run();
+  });
+
+  describe('#run', function(){
+
+    it('logs start info', function(){
+      expect(log.info).toHaveBeenCalled();
+    });
+
+    it('register interval based execution of service refresh', function(){
+      target.up(up);
+
+      // trigger first ttl services refresh
+      jasmine.Clock.tick(config.updateInterval);
+      // trigger second ttl services refresh
+      jasmine.Clock.tick(config.updateInterval);
+
+      // service should not be registered
+      var result = target.down(down);
+      expect(result.status).toBe(500);
+    });
+  });
+
+  describe('#run', function(){
+
+    it('logs stop info', function(){
+      expect(log.info).toHaveBeenCalled();
+    });
+
+    it('unregister interval based execution of service refresh', function(){
+      spyOn(jasmine.Clock.installed, 'clearInterval');
+      target.stop(up);
+      // trigger first ttl services refresh
+      jasmine.Clock.tick(config.updateInterval);
+
+      expect(jasmine.Clock.installed.clearInterval).toHaveBeenCalled();
+    });
+  });
+
+  describe('#up', function(){
+
+    describe('register the service instance', function() {
+
+      describe('with success', function(){
+
+        it('returns 200', function() {
+          var result = target.up(up);
+          expect(result.status).toBe(200);
+        });
+      });
+
+      describe('with error', function(){
+
+        it('returns 500 when instance is registered', function() {
+          target.up(up);
+
+          var result = target.up(up);
+          expect(result.status).toBe(500);
+        });
+
+        it('logs warning info when service is registered', function(){
+          target.up(up);
+          target.up(up);
+
+          expect(log.warn).toHaveBeenCalled();
+        });
+
+      });
+    });
+  });
+
+  describe('#down', function(){
+
+    beforeEach(function(){
+      target.up(up);
+    });
+
+    describe('unregister the service instance', function() {
+
+      describe('with success', function(){
+
+        it('returns 200', function() {
+          var result = target.down(down);
+          expect(result.status).toBe(200);
+        });
+      });
+
+      describe('with error', function(){
+
+        it('returns 500 when instance is not registered', function() {
+          target.down(down);
+
+          var result = target.down(down);
+          expect(result.status).toBe(500);
+        });
+
+        it('logs warning when service is not registered', function(){
+          target.down(down);
+          target.down(down);
+
+          expect(log.warn).toHaveBeenCalled();
+        });
+
+      });
+    });
+  });
+
+  describe('#heartbeat', function(){
+
+    describe('when instance not registered execute registration', function() {
+
+      describe('with success', function(){
+
+        it('returns 200', function() {
+          var result = target.heartbeat(heartbeat);
+          expect(result.status).toBe(200);
+        });
+
+        it('logs info about service registration', function(){
+          target.heartbeat(heartbeat);
+          expect(log.info).toHaveBeenCalled();
+        });
+      });
+
+    });
+
+    describe('refresh the service instance', function() {
+      beforeEach(function(){
+        target.up(up);
+      });
+
+      describe('with success', function(){
+
+        it('returns 200', function() {
+          var result = target.heartbeat(heartbeat);
+          expect(result.status).toBe(200);
+        });
+
+        it('updates service instance ttl', function() {
+          // trigger first ttl services refresh to reduce ttl
+          jasmine.Clock.tick(config.updateInterval);
+          // refresh ttl
+          target.heartbeat(heartbeat);
+          // trigger second ttl services refresh to reduce ttl
+          jasmine.Clock.tick(config.updateInterval);
+          // service should be registered
+          expect(target.up(up).status).toBe(500);
+        });
+
+        it('twice does not invalidate next heartbeat from being send', function() {
+          target.heartbeat(heartbeat);
+          target.heartbeat(heartbeat);
+
+          // trigger first ttl services refresh
+          jasmine.Clock.tick(config.updateInterval);
+          // trigger second ttl services refresh
+          jasmine.Clock.tick(config.updateInterval);
+
+          var result = target.down(down);
+          expect(result.status).toBe(500);
+        });
+      });
+
+    });
+
+  });
+
+  describe("#getInstance", function(){
+
+    describe("with single instance registered", function(){
+      it("returns the same", function(){
+        target.up(up);
+        var result = target.getInstance(up.payload);
+        expect(result).toBe(up.identity);
+      });
+    });
+
+    describe("with multiple instances registered", function(){
+      var instance1, instance2;
+
+      beforeEach(function(){
+        instance1 = up.identity;
+        target.up(up);
+        instance2 = up.identity + "2";
+        up.identity = instance2;
+        target.up(up);
+      });
+
+      it("returns the first instance", function(){
+        var result = target.getInstance(up.payload);
+        expect(result).toBe(instance1);
+      });
+
+      it("returns instance using load balancing algorithm", function(){
+        expect(target.getInstance(up.payload)).not
+          .toBe(target.getInstance(up.payload));
+      });
+
+    });
+
+    describe("with unregistered instances for sid", function(){
+      it("returns null", function(){
+        var result = target.getInstance(up.payload);
+        expect(result).toBe(null);
+      });
+    });
+  });
+
+});
