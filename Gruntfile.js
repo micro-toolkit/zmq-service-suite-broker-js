5cad8ce1c0f4876d2f5dda8638b8899a519c9e0c
Pedro Januário <prnjanuario@gmail.com>
2014-07-16 16:53:38 +0100

    added package bump tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,21 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
+    bump: {
+      options: {
+        files: ['package.json'],
+        updateConfigs: [],
+        commit: true,
+        commitMessage: 'Release v%VERSION%',
+        commitFiles: ['package.json'],
+        createTag: true,
+        tagName: 'v%VERSION%',
+        tagMessage: 'Version %VERSION%',
+        push: false,
+        gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d'
+      }
+    },
+
     watch: {
       files: '<%= jshint.files %>',
       tasks: ['jshint', 'unit']
