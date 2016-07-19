352570290995ea2afe92e53d04765ed66e0ce137
Pedro Januário <prnjanuario@gmail.com>
2016-07-19 22:17:36 +0100

    added after success hook to build docker image

---
+++ scripts/build_docker_image.sh
@@ -0,0 +1,12 @@
+if [ "$TRAVIS_BRANCH" == "master" ]; then
+  /bin/bash echo "Pushing to DockerHub..."
+  docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+  export REPO=microtoolkit/broker
+  export TAG=latest
+  docker build -f Dockerfile -t $REPO:$COMMIT .
+  docker tag $REPO:$COMMIT $REPO:$TAG
+  docker tag $REPO:$COMMIT $REPO:travis-$TRAVIS_BUILD_NUMBER
+  docker push $REPO
+else
+  /bin/bash echo "Docker image is built on master only"
+fi
