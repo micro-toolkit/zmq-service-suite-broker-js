bad42df77492695e25a10ff1934ac82a3d77a3aa
Pedro Januário <prnjanuario@gmail.com>
2018-01-04 18:01:14 +0000

    check properly if is a pr build

--- scripts/build_docker_image.sh
+++ scripts/build_docker_image.sh
@@ -1,6 +1,6 @@
-echo "BRANCH ${TRAVIS_BRANCH}"
+echo "PR JOB ${TRAVIS_PULL_REQUEST}"
 
-if [ "$TRAVIS_BRANCH" == "master" ]; then
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   echo "NODE VERSION ${TRAVIS_NODE_VERSION}"
   echo "Pushing to DockerHub..."
   docker login -u $DOCKER_USER -p $DOCKER_PASS
