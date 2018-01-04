72cf40c87dd55a3088f3ecb779889073918efb0b
Pedro Januário <prnjanuario@gmail.com>
2018-01-04 17:47:15 +0000

    docker login -e is not supported anymore

--- scripts/build_docker_image.sh
+++ scripts/build_docker_image.sh
@@ -1,7 +1,7 @@
 if [ "$TRAVIS_BRANCH" == "master" ]; then
   /bin/bash echo "NODE VERSION ${TRAVIS_NODE_VERSION}"
   /bin/bash echo "Pushing to DockerHub..."
-  docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+  docker login -u $DOCKER_USER -p $DOCKER_PASS
   export REPO=microtoolkit/broker
   export TAG=latest
   docker build -f Dockerfile -t $REPO:$COMMIT .
